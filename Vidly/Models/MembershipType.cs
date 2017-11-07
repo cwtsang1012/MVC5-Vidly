89ce3eeaebcfb41e99d0adb943145d768d4b2e9f
Grace Tsang <33336177+cwtsang1012@users.noreply.github.com>
2017-11-07 17:08:48 +0800

    Populate MembershipTypes Record

---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }  /* value from 1 to 12 */
+        public byte DiscountRate { get; set; } /* value from 0 to 100 */
+
+    }
+}
