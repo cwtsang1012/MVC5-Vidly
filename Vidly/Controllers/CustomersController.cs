79d461d3b1a0fcdea44158d4739e8713c440e25d
Grace Tsang <33336177+cwtsang1012@users.noreply.github.com>
2017-11-08 01:19:54 +0800

    Get Customers from database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,31 +10,34 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View(GetCustomers());
+            var customers = _context.Customers.ToList();
+            return View(customers);
         }
 
         [Route("Customers/Detail/{id}")]
         public ActionResult Detail(int id)
         {
-            var customers = GetCustomers();
-            var customer = customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             else
                 return View(customer);
 
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>()
-            {
-                new Customer(){ Id = 1, Name = "John Smith" },
-                new Customer(){ Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
