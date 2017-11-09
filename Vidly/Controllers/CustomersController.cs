e5d405c958701f81ed141da496b64eaa0c76a85f
Grace Tsang <33336177+cwtsang1012@users.noreply.github.com>
2017-11-09 12:20:33 +0800

    Rendering Drop-down List

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -25,8 +25,14 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
         }
+
 
         // GET: Customers
         public ActionResult Index()
