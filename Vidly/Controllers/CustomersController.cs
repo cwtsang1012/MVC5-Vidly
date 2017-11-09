431c6bda6caa9b6572f498269369a57e1a68fdb9
Grace Tsang <33336177+cwtsang1012@users.noreply.github.com>
2017-11-09 15:16:12 +0800

    Model Binding & Saving Data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -33,6 +33,31 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Create(Customer customer) {
+            /*
+             * MVC framework will automatically map the requested data to this input object 
+             * This is what we call Model Binding
+             * If we change the type of parameter to Customer, entity frameowrk is smart enough to bind this object to form data
+             * Because all the keys in the form data are prefixed
+             */
+            _context.Customers.Add(customer); /*write in memory, not in DB*/
+
+            /* our DbContext has a change tracking mechanism,
+             * so any time you add an object, modify the object or remove one of the existing objects
+             * it will mark them as either modified or deleted
+            */
+
+            _context.SaveChanges(); /* persist the changes*/
+            /* At this point our context goes through all modified objects and based on the kind of of modification,
+             * it will generate SQL statements at runtime and then run them on database.
+             * All these statements are wrapped in a transaction.
+             * So the changes get peristed together or nothing get persisted
+            */
+
+            return RedirectToAction("Index", "Customers");
+
+        }
 
         // GET: Customers
         public ActionResult Index()
