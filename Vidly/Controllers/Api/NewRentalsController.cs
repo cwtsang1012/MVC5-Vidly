8531801880eece9ad95256f7025e3720591bcfff
cwtsang1012 <cwtsang1012@gmail.com>
2018-04-11 18:08:19 +0800

    Building Simplest Api in NewRentalsController

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRentalDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
